62ac487d24e329012963f4dd5e57a3ad23f35ce4
fachoquecota <62406549+fachoquecota@users.noreply.github.com>
2024-03-19 16:29:43 -0500

    Reporte Excel añadido a Boleta Determinación

--- Unna.OperationalReport.WebSite/Controllers/Admin/IngenieroProceso/Reporte/Diario/BoletaDeterminacionVolumenController.cs
+++ Unna.OperationalReport.WebSite/Controllers/Admin/IngenieroProceso/Reporte/Diario/BoletaDeterminacionVolumenController.cs
@@ -11,6 +11,8 @@ using Aspose.Cells;
 using ClosedXML.Report;
 using Unna.OperationalReport.Tools.Comunes.Infraestructura.Dtos;
 using Unna.OperationalReport.Service.Reportes.ReporteDiario.BoletaDeterminacionVolumenGna.Dtos;
+using Unna.OperationalReport.Service.Reportes.ReporteDiario.BoletaVentaGns.Dtos;
+using Unna.OperationalReport.Tools.Comunes.Infraestructura.Utilitarios;
 
 namespace Unna.OperationalReport.WebSite.Controllers.Admin.IngenieroProceso.Reporte.Diario
 {
@@ -37,6 +39,84 @@ namespace Unna.OperationalReport.WebSite.Controllers.Admin.IngenieroProceso.Repo
         {
             var operacion = await _boletaDeterminacionVolumenGnaServicio.ObtenerAsync(ObtenerIdUsuarioActual() ?? 0);
             return ObtenerResultadoOGenerarErrorDeOperacion(operacion);
+        }
+
+        [HttpPost("Guardar")]
+        [RequiereAcceso()]
+        public async Task<RespuestaSimpleDto<bool>?> GuardarAsync(BoletaDeterminacionVolumenGnaDto boletaDeterminacionVolumenGna)
+        {
+            VerificarIfEsBuenJson(boletaDeterminacionVolumenGna);
+            boletaDeterminacionVolumenGna.IdUsuario = ObtenerIdUsuarioActual() ?? 0;
+            var operacion = await _boletaDeterminacionVolumenGnaServicio.GuardarAsync(boletaDeterminacionVolumenGna);
+            return ObtenerResultadoOGenerarErrorDeOperacion(operacion);
+        }
+
+        [HttpGet("GenerarExcel")]
+        [RequiereAcceso()]
+        public async Task<IActionResult> GenerarExcelAsync()
+        {
+            var operativo = await _boletaDeterminacionVolumenGnaServicio.ObtenerAsync(ObtenerIdUsuarioActual() ?? 0);
+            if (!operativo.Completado || operativo.Resultado == null)
+            {
+                return File(new byte[0], "application/octet-stream");
+            }
+            var dato = operativo.Resultado;
+            var factoresAsignacionGasCombustible = new
+            {
+                Items = dato.FactoresAsignacionGasCombustible
+            };
+
+            var factorAsignacionGns = new
+            {
+                Items = dato.FactorAsignacionGns
+            };
+
+            var factorAsignacionLiquidosGasNatural = new
+            {
+                Items = dato.FactorAsignacionLiquidosGasNatural
+            };
+
+            var complexData = new
+            {
+                Compania = dato?.General?.Nombre,
+                PreparadoPör = $"Preparado por: {dato?.General?.PreparadoPör}",
+                AprobadoPor = $"Aprobado por: {dato?.General?.AprobadoPor}",
+
+                DiaOperativo = dato?.Fecha,
+                VolumenTotalGasCombustible = dato?.VolumenTotalGasCombustible,
+                VolumenTotalGns = dato?.VolumenTotalGns,
+
+                FactoresAsignacionGasCombustible = factoresAsignacionGasCombustible,
+                FactorAsignacionGns = factorAsignacionGns,
+                VolumenProduccionTotalGlp = dato?.VolumenProduccionTotalGlp,
+                VolumenProduccionTotalCgn = dato?.VolumenProduccionTotalCgn,
+                VolumenProduccionTotalLgn = dato?.VolumenProduccionTotalLgn,
+                FactorAsignacionLiquidosGasNatural = factorAsignacionLiquidosGasNatural,
+                DistribucionGasNaturalAsociado = dato?.DistribucionGasNaturalAsociado,
+                VolumenProduccionTotalGlpLoteIv = dato?.VolumenProduccionTotalGlpLoteIv,
+                VolumenProduccionTotalCgnLoteIv = dato?.VolumenProduccionTotalCgnLoteIv,
+                FactorCoversion = dato?.FactorCoversion,
+
+                VolumenGnsVentaVgnsvTotal = dato?.VolumenGnsVentaVgnsvTotal ?? 0,
+                VolumenGnsVentaVgnsvEnel = dato?.VolumenGnsVentaVgnsvEnel ?? 0,
+                VolumenGnsVentaVgnsvGasnorp = dato?.VolumenGnsVentaVgnsvGasnorp ?? 0,
+                VolumenGnsVentaVgnsvLimagas = dato?.VolumenGnsVentaVgnsvLimagas ?? 0,
+                VolumenGnsFlareVgnsrf = dato?.VolumenGnsFlareVgnsrf ?? 0,
+                SumaVolumenGasCombustibleVolumen = double.IsNaN(dato?.SumaVolumenGasCombustibleVolumen ?? 0) ? 0 : dato.SumaVolumenGasCombustibleVolumen,
+                VolumenGnaFiscalizado = dato?.VolumenGnaFiscalizado ?? 0,
+
+            };
+            var tempFilePath = $"{_general.RutaArchivos}{Guid.NewGuid()}.xlsx";
+
+            using (var template = new XLTemplate($"{_hostingEnvironment.WebRootPath}\\plantillas\\reporte\\diario\\BoletaDeterminacionVolGNA.xlsx"))
+            {
+                template.AddVariable(complexData);
+                template.Generate();
+                template.SaveAs(tempFilePath);
+            }
+            var bytes = System.IO.File.ReadAllBytes(tempFilePath);
+            System.IO.File.Delete(tempFilePath);
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"BoletaDeterminacionVolGNA-{dato.Fecha.Replace("/", "-")}.xlsx");
         }
     }
 }
