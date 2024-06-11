fa9dea3181d3957cb2529228578d045eb027262f
Alan Choquesillo <achoquesillo@gmail.com>
2024-06-10 23:30:59 -0500

    .

--- Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
+++ Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9bfce6ed2ea61078ef7fa3769f72ec9cf65c070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db5934f6458f40f7377ca9a7c5afdbb2480a3dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
