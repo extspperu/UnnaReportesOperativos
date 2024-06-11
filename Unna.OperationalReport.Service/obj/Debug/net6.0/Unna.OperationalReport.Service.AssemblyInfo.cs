8bda2fa45f312a4fca82e2111361a6f6d25d128e
Alan Choquesillo <achoquesillo@gmail.com>
2024-06-11 00:29:26 -0500

    Bajada de Main a mi rama y correcion de reporte Composion LIV 2da quincena

--- Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
+++ Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db5934f6458f40f7377ca9a7c5afdbb2480a3dda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9dea3181d3957cb2529228578d045eb027262f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
