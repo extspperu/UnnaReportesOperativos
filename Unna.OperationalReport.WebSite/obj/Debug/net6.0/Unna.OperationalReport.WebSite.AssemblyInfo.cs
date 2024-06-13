d45e771b120ac8d559b520d34ceb78b2bef90cf9
Alan Choquesillo <achoquesillo@gmail.com>
2024-06-13 02:19:27 -0500

    Actualizacion de Reportes Anexo 7 con todas las observaciones y impresion de las 4 hojas en un excel

--- Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
+++ Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a292d5deaea0eb5a5276e84e48936f6f3f6faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f86a9ce88d5283a9f0b70d3b00de9f893af8d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
