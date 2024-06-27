f6586640d09ba9bd865bdeee4c8c50024f0cc8dc
mvillanueva <mvillanueva@orbelite.com>
2024-06-27 18:53:08 -0500

    registro osinergmin

--- Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
+++ Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0bff99ec4e69d26647d935a6c1e91d2536a52d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f060d8d14656849f3e175fb9fad2f80adf5f5e31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Unna.OperationalReport.WebSite/Controllers/Admin/IngenieroProceso/Osinergmin/RegistroController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Unna.OperationalReport.Service.Registros.Osinergmin.Dtos;
+using Unna.OperationalReport.Service.Registros.Osinergmin.Servicios.Abstracciones;
+using Unna.OperationalReport.Tools.Comunes.Infraestructura.Dtos;
+using Unna.OperationalReport.Tools.WebComunes.ApiWeb.Auth.Atributos;
+using Unna.OperationalReport.Tools.WebComunes.WebSite.Base;
+
+namespace Unna.OperationalReport.WebSite.Controllers.Admin.IngenieroProceso.Osinergmin
+{
+    [Route("api/ingenieroProceso/osinergmin/[controller]")]
+    [ApiController]
+    public class RegistroController : ControladorBase
+    {
+
+        private readonly IOsinergminServicio _osinergminServicio;
+        public RegistroController(IOsinergminServicio osinergminServicio)
+        {
+            _osinergminServicio = osinergminServicio;
+        }
+
+        [HttpPost("Guardar")]
+        [RequiereAcceso()]
+        public async Task<RespuestaSimpleDto<bool>?> GuardarAsync(OsinergminDto parametros)
+        {
+            VerificarIfEsBuenJson(parametros);
+            parametros.IdUsuario = ObtenerIdUsuarioActual();
+            parametros.Fecha = DateTime.UtcNow;
+            var operacion = await _osinergminServicio.GuardarAsync(parametros);
+            return ObtenerResultadoOGenerarErrorDeOperacion(operacion);
+        }
+
+
+        [HttpGet("Obtener")]
+        [RequiereAcceso()]
+        public async Task<OsinergminDto?> ObtenerAsync()
+        {
+            var operacion = await _osinergminServicio.ObtenerAsync(DateTime.UtcNow);
+            return ObtenerResultadoOGenerarErrorDeOperacion(operacion);
+        }
+    }
+}
