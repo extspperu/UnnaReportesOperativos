771ecb2687a59041f319cafb00264e72fffb1f44
fachoquecota <62406549+fachoquecota@users.noreply.github.com>
2024-07-02 09:26:39 -0500

    módulo de laboratorio

---
+++ Unna.OperationalReport.WebSite/Pages/Admin/Laboratorio/GNSGNA/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Unna.OperationalReport.WebSite.Pages.Admin.Laboratorio.GNS_GNA
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Unna.OperationalReport.WebSite/Pages/Admin/Laboratorio/Index.cshtml.cs
+++ Unna.OperationalReport.WebSite/Pages/Admin/Laboratorio/Index.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Unna.OperationalReport.Service.Configuraciones.MenuUrls.Servicios.Abstracciones;
 
 namespace Unna.OperationalReport.WebSite.Pages.Admin.Laboratorio
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public string? NombreMenu { get; set; }
+
+        private readonly IMenuUrlServicio _menuUrlServicio;
+        public IndexModel(IMenuUrlServicio menuUrlServicio)
         {
+            _menuUrlServicio = menuUrlServicio;
+        }
+        public string? IdGrupo { get; set; }
+        public async Task OnGet(string? Id)
+        {
+            IdGrupo = Id;
+
+            var operacion = await _menuUrlServicio.ObtenerListaMenuUrl(Id);
+            if (operacion != null && operacion.Completado && operacion.Resultado != null)
+            {
+                NombreMenu = operacion.Resultado.Nombre;
+            }
         }
     }
 }
