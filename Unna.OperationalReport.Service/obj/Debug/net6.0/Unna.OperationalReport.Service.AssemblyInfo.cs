4d7d7d00123f841bf846c79eeb25cf8f99c33392
Alan Choquesillo <achoquesillo@gmail.com>
2024-07-08 13:28:07 -0500

    assamby

--- Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
+++ Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc08f394a01d8f986789b6a44eb47cbaf5061685")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b2ad8c818a0dbe5158a7c688e80bfda075f9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
