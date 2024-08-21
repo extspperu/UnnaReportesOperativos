c3358aed04e6b3eead4a44ef7d8d62255a3948a6
fachoquecota <62406549+fachoquecota@users.noreply.github.com>
2024-08-21 09:14:44 -0500

    ajuste reporte

--- Unna.OperationalReport.WebSite/wwwroot/js/Admin/IngenieroProceso/Historico/index.js
+++ Unna.OperationalReport.WebSite/wwwroot/js/Admin/IngenieroProceso/Historico/index.js
@@ -4,55 +4,206 @@ $(document).ready(function () {
     });
 
 function controles() {
-    $('#btn-3_GNA').click(() => descargarReporte('3_GNA'));
-        $('#btn-1b_Tks').click(() => descargarReporte('1b_Tks'));
-        $('#btn-3_Gas').click(() => descargarReporte('3_Gas'));
-        $('#btn-Existencias').click(() => descargarReporte('Existencias'));
-        $('#btn-2_Líquidos').click(() => descargarReporte('2_Líquidos'));
-Obtener();
+
+        $('#btn3_GNA').click(function () {
+            console.log("Iniciando EXCEL");
+
+            descargar3_GNA();
+        });
+        $('#btn1b_Tks').click(function () {
+            console.log("Iniciando EXCEL");
+
+            descargar1b_Tks();
+        });
+        $('#btn3_Gas').click(function () {
+            console.log("Iniciando EXCEL");
+
+            descargar3_Gas();
+        });
+        $('#btnExistencias').click(function () {
+            console.log("Iniciando EXCEL");
+
+            descargarExistencias();
+        });
+        $('#btn2_Liquidos').click(function () {
+            console.log("Iniciando EXCEL");
+
+            descargar2_Liquidos();
+        });
+        Obtener();
     }
 
 function Obtener() {
-        var url = $('#__URL_OBTENER').val();
-console.log("Intentando ingresar al url ", url);
-var dato = { };
-realizarGet(url, dato, 'json', RespuestaObtener, ErrorObtener, 10000);
-    }
+    var url = $('#__URL_OBTENER').val();
+    console.log("Intentando ingresar al url ", url);
+    var dato = { };
+    realizarGet(url, dato, 'json', RespuestaObtener, ErrorObtener, 10000);
+}
 
 function RespuestaObtener(data) {
     console.log(data);
-    }
+}
 
 function ErrorObtener(data) {
-    console.log(data);
-$("#contenidoErrorMensaje").show();
-    }
+        console.log(data);
+        $("#contenidoErrorMensaje").show();
+}
 
-function descargarReporte(tipoReporte) {
-        var urlBase = $('#__URL_OBTENER').val();
-var url = `${urlBase}?tipoReporte=${encodeURIComponent(tipoReporte)}`;
+function descargar3_GNA() {
+    // Mostrar el GIF de carga
+    $('#btn3_GNA img.loading').show();
 
-$('#btn-' + tipoReporte).find('img.loading').show();
+    // Realizar la descarga
+    var url = $("#__URL_EXCEL_3_GNA").val();
 
-fetch(url)
-            .then(response => response.blob())
-            .then(blob => {
-                const url = window.URL.createObjectURL(blob);
-const a = document.createElement('a');
-a.style.display = 'none';
-a.href = url;
-a.download = `${tipoReporte}.xlsx`;
-document.body.appendChild(a);
-a.click();
-window.URL.revokeObjectURL(url);
+    $.ajax({
+        type: 'GET',
+        url: url,
+        xhrFields: {
+            responseType: 'blob' 
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = '3_GNA.xlsx'; 
+            document.body.append(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error en la descarga", errorThrown);
+        },
+        complete: function () {
+            $('#btn3_GNA img.loading').hide();
+        }
+    });
+}
+function descargar1b_Tks() {
+    // Mostrar el GIF de carga
+    $('#btn1b_Tks img.loading').show();
 
-$('#btn-' + tipoReporte).find('img.loading').hide();
-            })
-            .catch(error => {
-    console.error('Error al generar el reporte:', error);
-$('#btn-' + tipoReporte).find('img.loading').hide();
-            });
-    }
+    // Realizar la descarga
+    var url = $("#__URL_EXCEL_1b_Tks").val();
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = '1b_Tks.xlsx';
+            document.body.append(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error en la descarga", errorThrown);
+        },
+        complete: function () {
+            $('#btn1b_Tks img.loading').hide();
+        }
+    });
+}
+function descargar2_Liquidos() {
+    // Mostrar el GIF de carga
+    $('#btn2_Liquidos img.loading').show();
+
+    // Realizar la descarga
+    var url = $("#__URL_EXCEL_2_Liquidos").val();
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = '2_Liquidos.xlsx';
+            document.body.append(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error en la descarga", errorThrown);
+        },
+        complete: function () {
+            $('#btn2_Liquidos img.loading').hide();
+        }
+    });
+}
+function descargar3_Gas() {
+    // Mostrar el GIF de carga
+    $('#btn3_Gas img.loading').show();
+
+    // Realizar la descarga
+    var url = $("#__URL_EXCEL_3_Gas").val();
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = '3_Gas.xlsx';
+            document.body.append(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error en la descarga", errorThrown);
+        },
+        complete: function () {
+            $('#btn3_Gas img.loading').hide();
+        }
+    });
+}
+function descargarExistencias() {
+    // Mostrar el GIF de carga
+    $('#btnExistencias img.loading').show();
+
+    // Realizar la descarga
+    var url = $("#__URL_EXCEL_Existencias").val();
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = 'Existencias.xlsx';
+            document.body.append(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error en la descarga", errorThrown);
+        },
+        complete: function () {
+            $('#btnExistencias img.loading').hide();
+        }
+    });
+}
 
 function realizarGet(url, dato, tipo, Funcion, FuncionError, timeout) {
     $.ajax({
