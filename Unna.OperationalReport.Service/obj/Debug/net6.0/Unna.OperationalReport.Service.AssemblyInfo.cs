e5ccc61bc58f0e4004ffc748e59928fce933cfca
mvillanueva <mvillanueva@orbelite.com>
2024-08-28 14:19:20 -0500

    correciones

--- Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
+++ Unna.OperationalReport.Service/obj/Debug/net6.0/Unna.OperationalReport.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5feb70f562e9fec0cc0bc2f6651ae7cab06aec98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8ea915c55c7961bb48549bd2b1f54f99567cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
+++ Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2475f5145ccd1dbaa9bc028cb3932858abc5e24d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8ea915c55c7961bb48549bd2b1f54f99567cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
