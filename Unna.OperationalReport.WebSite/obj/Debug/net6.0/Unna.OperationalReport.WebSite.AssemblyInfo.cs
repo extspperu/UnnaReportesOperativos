bdb0592a9c77c168b94013800437abd9bcfa189b
mvillanueva <mvillanueva@orbelite.com>
2024-09-04 10:50:05 -0500

    dia operativo 01-07

--- Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
+++ Unna.OperationalReport.WebSite/obj/Debug/net6.0/Unna.OperationalReport.WebSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+febcf578982176bfdf875b150d372f3928674264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b810a3c78c398157367496a4f541bc9d3ac106")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unna.OperationalReport.WebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
